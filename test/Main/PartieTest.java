ea3bf705eb300b79152101f7c5e9321f45074988
Warren <wazaa8@gmail.com>
2015-10-20 22:13:30 +0200

    Ajout du fichier de Test unitaire

---
+++ test/Main/PartieTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Warren
+ */
+public class PartieTest {
+    
+    @Before
+    public void PartieTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testGetGain(){
+        
+    }
+    
+    public void testGetScore(){
+        
+    }
+    
+    public void testCoup(){
+        
+    }
+    
+    public void testACooperrer(){
+        
+    }
+    
+}
