aecaf9b8e20bbe6df0192c3bc39f0f3b3d46f8f6
florent <florent@10.10.22.186>
2015-10-21 11:38:22 +0200

    Création Classe Joueur Partie et Modification de la classe partieIHMDilemme  Base1

--- src/Main/IHMDilemme.java
+++ src/Main/IHMDilemme.java
@@ -7,7 +7,7 @@ package Main;
 
 /**
  *
- * @author Warren
+ * @author florent
  */
 public class IHMDilemme extends javax.swing.JFrame {
 
@@ -27,21 +27,171 @@ public class IHMDilemme extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("C | F");
+
+        jButton2.setText("F | F");
+
+        jButton3.setText("C | C");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("F | C");
+
+        jLabel1.setText("Coopere");
+
+        jLabel2.setText("Fait Defaut");
+
+        jLabel3.setText("Coopere");
+
+        jLabel4.setText("Fait Defaut");
+
+        jLabel5.setText("A | B");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(23, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jButton4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton2))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel2)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton3)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton4)
+                    .addComponent(jLabel4))
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 187, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 148, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 470, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jLabel6.setText("Synthèse");
+
+        jLabel7.setText("Prochain Coup");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel7)))
+                .addGap(24, 24, 24)
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 69, Short.MAX_VALUE)
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27)))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        Partie partie = new Partie();
+        partie.cooperer(true, true);
+        assertTrue(partie.getACooperer(2,1)); //param : numero manche,joueur 
+        // TODO: 5 méthodes : getGain(),Getnbrcoup,getScoreAOUB()
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -69,6 +219,9 @@ public class IHMDilemme extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(IHMDilemme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -79,5 +232,19 @@ public class IHMDilemme extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
--- src/Main/Partie.java
+++ src/Main/Partie.java
@@ -5,10 +5,105 @@
  */
 package Main;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Warren
+ * @author florent
  */
-public class Partie {
+public class Partie extends Joueur {
+    private ArrayList<String[]> manche; 
+    private int nbrcoups;
+    private Joueur joueur1;
+    private Joueur joueur2;
     
+    
+    public Partie()
+    {
+        this.joueur1 = new Joueur();
+        this.joueur2 = new Joueur();
+        this.manche = new ArrayList<String[]>();
+    }
+    
+    public boolean getACooperer(int manchetmp, int numero_joueur)
+    {
+        String[] coup = this.manche.get(manchetmp);
+        if(coup[numero_joueur].equals("Cooperer"))
+            return true;
+        else if(coup[numero_joueur].equals("FaitDefaut"))
+            return false;
+        return false;
+    }
+    
+    public void cooperer(boolean joueur1, boolean joueur2)
+    {
+        String[] choix = new String[2];
+        if(joueur1 && joueur2)
+        {            
+            choix [0]= "Cooperer";
+            choix [1]= "Cooperer";
+            this.manche.add(choix);
+        }else if(!joueur1 && joueur2)
+        {            
+            choix [0]= "FaitDefaut";
+            choix [1]= "Cooperer";
+            this.manche.add(choix);
+        }else if(joueur1 && !joueur2)
+        {            
+            choix [0]= "Cooperer";
+            choix [1]= "FaitDefaut";
+            this.manche.add(choix);
+        }else if(!joueur1 && !joueur2)
+        {            
+            choix [0]= "FaitDefaut";
+            choix [1]= "FaitDefaut";
+            this.manche.add(choix);
+        }
+    }
+    
+    public int getScoreJoueur(int numero_Joueur)
+    {
+        if(numero_Joueur == 0)
+            return this.joueur1.getScore();
+        else if(numero_Joueur == 1)
+            return this.joueur2.getScore();
+        return -1;
+    }
+    
+    public int getNbrCoupJoueur(int numero_Joueur)
+    {
+        if(numero_Joueur == 0)
+            return this.joueur1.getNbrCoup();
+        else if(numero_Joueur == 1)
+            return this.joueur2.getNbrCoup();
+        return -1;
+    }        
+    public int[] getGain(int gainManche, int numero_Joueur)
+    {
+        String[] manchetmp = this.manche.get(gainManche);
+        int[] tab = new int[2];
+        if(manchetmp[numero_Joueur].equals("Cooperer") && manchetmp[numero_Joueur].equals("Cooperer"))
+        {
+            tab[0] = 1;
+            tab[1] = 1;
+            return tab;
+        } else if(manchetmp[numero_Joueur].equals("Cooperer") && manchetmp[numero_Joueur].equals("FaitDefaut"))
+        {
+            tab[0] = 0;
+            tab[1] = 5;
+            return tab;
+        }else if(manchetmp[numero_Joueur].equals("FaitDefaut") && manchetmp[numero_Joueur].equals("Cooperer"))
+        {
+            tab[0] = 5;
+            tab[1] = 0;
+            return tab;
+            
+        }else if(manchetmp[numero_Joueur].equals("FaitDefaut") && manchetmp[numero_Joueur].equals("FaitDefaut"))
+        {
+            tab[0] = 3;
+            tab[1] = 3;
+            return tab;
+        }
+        return null;
+    }
 }
